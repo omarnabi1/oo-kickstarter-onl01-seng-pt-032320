e2d929abd8676e1205e32d2495b07a50905a76ec
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:23:06 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project 
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title 
+    @backers = [ ]
+  end
+  
+  def add_backer(backer)
+    @backer.push(backer)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+end
